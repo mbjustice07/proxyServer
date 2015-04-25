072f9b3336a607541fde40dd181cd34a0b283faf
Max Justice <mbjustice07@github.com>
2015-04-25 12:01:43 -0700

    protocol.h

--- protocol.h
+++ protocol.h
@@ -1,4 +1,39 @@
+//Alejandro Nicolette and Maxwell Justice
+#include <assert.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <sys/select.h>
+#include <pthread.h>
 
+#include "linkedList.h"
+
+#define CLIENT_PORT 5200
+#define LOCALHOST "127.0.0.1"
+
+#define SERVER_PORT 620
+
+#define SPROXY_LOCAL 23
+
+#define HEARTBEAT_LIMIT 3
+
+#ifdef DEBUG
+#define DB(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define DB(...) ((void)0)
+#endif
+
+
+#ifdef _LINUX_
+#include <stdint.h>
+#endif /* _LINUX_ */
+
+#include <sys/types.h>
+#include <arpa/inet.h>
 
 #ifndef PX_HEART
 #define PX_HEART        0x0202
@@ -12,13 +47,17 @@
 #define PX_DAT          0x0303
 #endif
 
-struct px
+#ifndef IP_MAXPACKET
+#define IP_MAXPACKET 65535
+#endif
+
+typedef struct px
 {
-        uint32_t px_ses;        // session number for extra credit
         uint32_t px_newaddr;    // the new connection initiation
         uint16_t px_tos;         // type of service
         uint16_t px_len;        // total length of packet
         uint32_t px_ack;        // acknowlegdement numbers
         uint32_t px_seq;        // sequence number 
-};
+        uint32_t px_ses;        // session number for extra credit
+} px;
 
